ae4a02ecdfc452ba71dc47a854f09c3fa4fa2100
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-10-18 12:43:06 +0300

    updates

--- src/main/java/gr/aueb/cf/springteacherapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/springteacherapp/mapper/Mapper.java
@@ -1,8 +1,11 @@
 package gr.aueb.cf.springteacherapp.mapper;
 
+import gr.aueb.cf.springteacherapp.core.enums.Role;
 import gr.aueb.cf.springteacherapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.springteacherapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.springteacherapp.dto.UserInsertDTO;
 import gr.aueb.cf.springteacherapp.model.Teacher;
+import gr.aueb.cf.springteacherapp.model.User;
 import org.springframework.stereotype.Component;
 
 
@@ -21,5 +24,10 @@ public class Mapper {
         return new TeacherReadOnlyDTO(teacher.getId(), teacher.getCreatedAt(),
                 teacher.getUpdatedAt(), teacher.getUuid(), teacher.getFirstname(),
                 teacher.getLastname(),teacher.getVat(), teacher.getRegion().getName());
+    }
+
+    public User mapToUserEntity(UserInsertDTO userInsertDTO) {
+        return new User(null, userInsertDTO.getUsername(),
+                userInsertDTO.getPassword(), Role.valueOf(userInsertDTO.getRole().toUpperCase()));
     }
 }
--- src/main/java/gr/aueb/cf/springteacherapp/service/UserService.java
+++ src/main/java/gr/aueb/cf/springteacherapp/service/UserService.java
@@ -4,6 +4,7 @@ import gr.aueb.cf.springteacherapp.model.User;
 import gr.aueb.cf.springteacherapp.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,10 +14,17 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     @Transactional
     public Optional<User> findUserByUsername(String username) {
         return userRepository.findByUsername(username);
+    }
+
+    @Transactional(rollbackOn = Exception.class)
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 }

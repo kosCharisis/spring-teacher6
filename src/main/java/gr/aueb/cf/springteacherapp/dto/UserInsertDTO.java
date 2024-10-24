84b5260a692bf2b52db0e773feab0de02c7e96b7
Konstantinos Charisis <harisis_kostas@hotmail.com>
2024-10-24 18:13:30 +0300

    logs

--- src/main/java/gr/aueb/cf/springteacherapp/dto/UserInsertDTO.java
+++ src/main/java/gr/aueb/cf/springteacherapp/dto/UserInsertDTO.java
@@ -12,12 +12,23 @@ import lombok.Setter;
 @Setter
 public class UserInsertDTO {
 
-    @Email
-    private String username;
+//    @NotEmpty(message = "Username is required")
+//    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+//    private String username;
 
-    @NotNull
-    @Pattern(regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\\d)(?=.*?[@$!%*?&]).{5,}$", message = "Invalid password")
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
 
-    private String role;
+    private String role; // assuming you want the user to select a role
+
+
+    @Email
+    private String username;
+//
+//    @NotNull
+//    @Pattern(regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\\d)(?=.*?[@$!%*?&]).{5,}$", message = "Invalid password")
+//    private String password;
+//
+//    private String role;
 }
